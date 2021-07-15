de57121d1ffea791f8efb63a740def97cffd5418
Tharushini Jayathilaka <67948704+TharuJayathilaka@users.noreply.github.com>
2021-07-15 23:52:59 +0530

    Add files via upload

---
+++ Lab/linked.c
@@ -0,0 +1,45 @@
+#include "linked.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stddef.h>
+
+/********************************
+ * add to head. Return 0 on success
+ ********************************/
+int head_insert(void *data, list_t *list)
+{
+  node_t*tmp;  // tmp is the new head add to the array
+  tmp=(node_t*)malloc(sizeof(node_t));
+  if(tmp){   //if the head is not null
+     tmp->data=data;  // get me only the data part
+     tmp->next=*list;  //get me only the link of the next data
+     *list=tmp;
+     return 0;
+  }else{
+      return -1;
+  }
+}
+
+
+/********************************
+ * Remove
+ * Store value in data, free node and return 1
+ ********************************/
+int remove_list(void **data, list_t *list)
+{
+  if(*list){  //if the first address is not null
+    node_t*tmp=*list;
+    //node_t*holder;
+    *data=tmp->data;
+    //holder=tmp->next;=
+    *list=(tmp)->next; // get the address of next data
+    free(tmp);  // free the node
+    tmp=NULL;   // should free the node as well as the data stored in the node.
+    return 1;
+  }
+  return 0;
+}
+
+
+
---
+++ Lab/main.c
@@ -0,0 +1,41 @@
+#include "linked.h"
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+#define MAX 5 // max number of cities.
+#define NAMESIZE 20 // names should have this many or less chars
+
+int main()
+{
+  list_t list = NULL;
+  char *city;
+
+  printf("Enter city names. Enter \"END\" to break\n");
+
+//  city = (char *)malloc(sizeof(char) * NAMESIZE);
+//  assert(city);
+  int noCity=0;
+  while(1) {
+        city = (char *)malloc(sizeof(char) * NAMESIZE);
+        assert(city);
+        if(scanf("%s", city)>0)
+        if(!strcmp(city, "END")) break; // break out of loop
+           noCity++;
+           assert(!head_insert(city, &list));
+  }
+  if(noCity>MAX){
+     printf("The no of cities that you can add is 5.");
+     return 0;
+  }
+
+  printf("Your way back: ");
+  while(remove_list((void **)&city, &list)){
+      printf("%s -> ", city);
+  }
+  printf("Thats it\n");
+  return 0;
+}
+
